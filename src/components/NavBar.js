7abed5b1816d1882c67355068ced8d2ca02bcf55
Celinesco <celina.alsina@gmail.com>
2022-03-02 14:20:09 +0100

    componente navBar, instalacion de sass, archivo variables.scss

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import './NavBar.scss'
+
+const NavBar = () => {
+    return (
+        <nav className="navbar">
+            <ul className="navbar__list">
+                <li>Home</li>
+                <li>Projects</li>
+                <li>About</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar;
