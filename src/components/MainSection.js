1d4eaa7ce8b49b89e37a6af9b38108c68c5b0617
Celinesco <celina.alsina@gmail.com>
2022-03-04 14:19:42 +0100

    imagen de perfil portada

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -5,7 +5,8 @@ import "aos/dist/aos.css";
 import { useEffect } from 'react';
 import ProjectsContainer from './ProjectsContainer';
 import AnimacionPortada from './AnimacionPortada';
-
+import profileImg from '../assets/profile.jpg';
+import ArnoldyYo from '../assets/ArnoldyYo.jpg'
 
 
 const MainSection = () => {
@@ -19,11 +20,18 @@ const MainSection = () => {
         <>
             <section className="section__main">
                 <div className='container__main-section'>
+                    <div className='secret-container'>
+                        <p>This is me and one of the best cats in the world.</p>
+                        <div className='container_img-profile'>
+                            <img src={ArnoldyYo}></img>
+                        </div>
+                    </div>
+
                     <AnimacionPortada />
-        
+
                 </div>
             </section>
-         
+
             <TechnologiesContainer />
             <ProjectsContainer />
         </>
--- src/components/ProjectInfo.js
+++ src/components/ProjectInfo.js
@@ -1,4 +1,5 @@
-import './DetailSection.scss'
+import './DetailSection.scss';
+import { HiOutlineExternalLink } from "react-icons/hi";
 
 const ProjectInfo = ({ title, description, technologies, link, code }) => {
     return (
@@ -9,9 +10,9 @@ const ProjectInfo = ({ title, description, technologies, link, code }) => {
                 <p>The project was built using: {technologies}.</p>
             </div>
             <div className='paragraph'>
-                <p><a href={link} target="_blank" rel="noopener noreferrer">_Click HERE! </a>to see the project.</p>
+                <p><a href={link} target="_blank" rel="noopener noreferrer"><HiOutlineExternalLink />Click HERE! </a>to see the project.</p>
             </div>
-            <p>_If you are interested in the code, <a href={code} target="_blank" rel="noopener noreferrer">you can also take a look at the repository.</a></p>
+            <p><HiOutlineExternalLink />If you are interested in the code, <a href={code} target="_blank" rel="noopener noreferrer">you can also take a look at the repository.</a></p>
         </article>
     )
 }
