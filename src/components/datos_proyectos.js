e8dbe5a1364b503e7c3ebdadde1aad80e2f0e95e
Celinesco <celina.alsina@gmail.com>
2022-03-07 09:27:26 +0100

    corrijo ingles de las descripciones de los proyectos

--- src/components/datos_proyectos.js
+++ src/components/datos_proyectos.js
@@ -5,7 +5,7 @@ export const datosProyecto = [
         img: "https://blogger.googleusercontent.com/img/a/AVvXsEjiy93dYLcllgGSHXnsr_4WMOkjBfz9fP8octsENo1KW7A3l1ejF7Im-Ns8wJRM-l44nG7X6DBC0INPaanNjvAoOoB85DVEURnoKBNu40BZtYZCUr0izz7k785Sw6vehAJtB7LTdG8peKDhEjcEeqZ-eoXOQPpNhpopo9VNJABWrE-VigtYZsMXFiCYLw=s320",
         alt: "code with many errors",
         link: "https://celinesco.github.io/Portafolio/",
-        description: "This is the first assignment of the Bootcamp ADA. The porftolio originally had only CSS and HTML, because those where the two first languages we learnt. In the second part of the course, I added some JS in order to make the dark-mode and the dropwdown menu for mobile. ",
+        description: "This is the first assignment of the ADA Bootcamp. The portfolio was originally based on only CSS and HTML, because those were the two first languages we learnt. In the second part of the course, I added some JS in order to make the dark-mode and the drop-down menu for mobile.",
         technologies: "HTML5, CSS3, JS",
         repository: "https://github.com/Celinesco/Portafolio"
     },
@@ -15,7 +15,7 @@ export const datosProyecto = [
         img: "https://i.etsystatic.com/32248327/r/il/614b2e/3442381236/il_340x270.3442381236_jxj5.jpg",
         alt: "Famous cat meme",
         link: "https://carosanchezl.github.io/Editor-de-memes/",
-        description: "This is the second project of the Boootcamp ADA, but the first group-assignment. With this project we learnt how to work on a collaborative project using GitHub, how to use external libraries and to make dinamical web pages manipulating the DOM with JS.",
+        description: "This is the second project of the ADA bootcamp, and the first group assignment. With this project I learnt how to work on a collaborative project with GitHub, how to use external libraries, and to make dynamical web pages by manipulating the DOM with JS.",
         technologies: "HTML5, CSS3, JS",
         repository: "https://github.com/CaroSanchezL/Editor-de-memes",
     },
@@ -25,7 +25,7 @@ export const datosProyecto = [
         img: "https://blogger.googleusercontent.com/img/a/AVvXsEiq822jcy76XX9xUD5vVqneksee2EbKYJ1zGJ8fH1bhaqT9yRTFh_Q97QaghXcUD5TzbIe4_2yX7Nd7VbIgjmbzAX18bV_Rg5OU7TuA8hyqt74yPyEP-h1pEbFtpUtF7SzRKKijQCplPKwIcRbU0aW6Du154r10i1SaimyE16a7yIW1Xz0lBTPdSuShzQ=w323-h399",
         alt: "tic-tac-toe game",
         link: "https://celinesco.github.io/MatcheADAs/",
-        description: "This was an optional project of the Bootcamp ADA. It is 3-match game. Although it has many things to improve(cleaning code,  it needs some extra functionalities, the responsive design and some bugs), it was really challenging and I'm happy with the results.",
+        description: "This was an optional project of the ADA bootcamp. It is a 3-match game. Some things are worth improving (cleaning the code + adding some extra functionalities, the responsive design and some bugs), it was really challenging and I am happy with the results.",
         technologies: "HTML5, CSS3, JS",
         repository: "https://github.com/Celinesco/MatcheADAs"
     },
@@ -35,7 +35,7 @@ export const datosProyecto = [
         img: "https://blogger.googleusercontent.com/img/a/AVvXsEjvvYItYKdgI-8jDOJJTmnqgeEcnZyAer7eBFwDVJIl1uEo3voUNf7k2GeAIvIkReNTkA--PNiZo8gficbgv0pXyRQIYKMoKN3xLxZ9dBZO3Dezde5cH2Vrry5xky6BPff8COKs_DGGHED8876GDMEcAQbbTGqO-1hJTgNff9lonFSY_TgXwUv0bFYdhA=s320",
         alt: "Statistics graph",
         link: "https://celinesco.github.io/AhorrAdas/",
-        description: "3rd project of the bootcamp ADA. This was also a group assignment. It's a tracker for your incomes and expenses. We learnt here, how to store the information user, on Local Storage.",
+        description: "The third project of the ADA bootcamp was also a group assignment. It is a tracker for your incomes and expenses. Here we learnt how to store the user information on Local Storage.",
         technologies: "HTML5, BULMA, JS",
         repository: "https://github.com/Celinesco/AhorrAdas",
     },
@@ -45,7 +45,7 @@ export const datosProyecto = [
         img: "https://blogger.googleusercontent.com/img/a/AVvXsEg2_0yrTTO2xuQm7hrMKIJNCELT70jXypEOW6NXhySMSqGgkLDJvnjPP4qQnbD_-yrLZP1qw8hIInAKinZamEVdIC3lc9EkAZJkJoAurRqAw0gvC-R6NPHt5gVmnrGMWYA3chptGvCn6OlL5XICv17iBbYIrW_f4Q5TKJQgc7XdMS4Hfi4RysF6ZdnPIg=s320",
         alt: "Kapow exclamation",
         link: "https://celinesco.github.io/buscador_de_comics_2/",
-        description: "Fourth proyect of the Bootcamp ADA. We learnt how to use publics APIs. I chose Marvel and Superhero APIs.",
+        description: "The aim of the project was to work with a public API. As always, we had pages examples, with different APIs. I chose Marvel Api, and also dabbled in another API to complete with those superheroes that don't belong to the Marvel world.",
         technologies: "HMTL, SASS, JS",
         repository: "https://github.com/Celinesco/buscador_de_comics_2"
     },
@@ -55,7 +55,7 @@ export const datosProyecto = [
         img: "https://blogger.googleusercontent.com/img/a/AVvXsEjelRjxXPLIpTBBUeEUQcnlbZ3R5n7mSBUeZr1zQXfH8EIxndWlDpRrhPSr5n_ENVJrSCyZhLxRjO-84AOOWRF52-0X_Blr0aONvLxbz4BXx3h-cvhuhBNXZFnm1GB2ftKryDzz7g9Udgo92doqXQz5l0z8Csdal_zt7KESMXCTbZO9HC6PMxzh8NGgHA=s320",
         alt: "Rick and Morty silhouette",
         link: "https://morty-and-rick-page.netlify.app/RickAndMortyPage",
-        description: "This page(SPA) was the result of an excercise. We were taught how to use APIs with React and to make pages more navigables with React-router-dom.",
+        description: "This page (SPA) was the result of an exercise. We were taught how to use APIs with React and to make pages more navigable with React Router DOM.",
         technologies: "React.js",
         repository: "https://github.com/Celinesco/RickAndMortyPage"
     },
@@ -74,3 +74,21 @@ export const datosProyecto = [
 
 
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
