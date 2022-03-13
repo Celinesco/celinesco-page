8eba2212a7e5f5e6541f7b739ab9a150cc44cbf1
Celinesco <celina.alsina@gmail.com>
2022-03-13 09:39:24 +0100

    agrego link de ultimo proyecto y cambio el nombre de ada a Ada ITW

--- src/components/datos_proyectos.js
+++ src/components/datos_proyectos.js
@@ -5,7 +5,7 @@ export const datosProyecto = [
         img: "https://blogger.googleusercontent.com/img/a/AVvXsEjiy93dYLcllgGSHXnsr_4WMOkjBfz9fP8octsENo1KW7A3l1ejF7Im-Ns8wJRM-l44nG7X6DBC0INPaanNjvAoOoB85DVEURnoKBNu40BZtYZCUr0izz7k785Sw6vehAJtB7LTdG8peKDhEjcEeqZ-eoXOQPpNhpopo9VNJABWrE-VigtYZsMXFiCYLw=s320",
         alt: "code with many errors",
         link: "https://celinesco.github.io/Portafolio/",
-        description: "This is the first assignment of the ADA Bootcamp. The portfolio was originally based on only CSS and HTML, because those were the two first languages we learnt. In the second part of the course, I added some JS in order to make the dark-mode and the drop-down menu for mobile.",
+        description: "This is the first assignment of the ADA ITW Bootcamp. The portfolio was originally based on only CSS and HTML, because those were the two first languages we learnt. In the second part of the course, I added some JS in order to make the dark-mode and the drop-down menu for mobile.",
         technologies: "HTML5, CSS3, JS",
         repository: "https://github.com/Celinesco/Portafolio"
     },
@@ -15,7 +15,7 @@ export const datosProyecto = [
         img: "https://i.etsystatic.com/32248327/r/il/614b2e/3442381236/il_340x270.3442381236_jxj5.jpg",
         alt: "Famous cat meme",
         link: "https://carosanchezl.github.io/Editor-de-memes/",
-        description: "This is the second project of the ADA bootcamp, and the first group assignment. With this project I learnt how to work on a collaborative project with GitHub, how to use external libraries, and to make dynamical web pages by manipulating the DOM with JS.",
+        description: "This is the second project of the ADA ITW bootcamp, and the first group assignment. With this project I learnt how to work on a collaborative project with GitHub, how to use external libraries, and to make dynamical web pages by manipulating the DOM with JS.",
         technologies: "HTML5, CSS3, JS",
         repository: "https://github.com/CaroSanchezL/Editor-de-memes",
     },
@@ -25,7 +25,7 @@ export const datosProyecto = [
         img: "https://blogger.googleusercontent.com/img/a/AVvXsEiq822jcy76XX9xUD5vVqneksee2EbKYJ1zGJ8fH1bhaqT9yRTFh_Q97QaghXcUD5TzbIe4_2yX7Nd7VbIgjmbzAX18bV_Rg5OU7TuA8hyqt74yPyEP-h1pEbFtpUtF7SzRKKijQCplPKwIcRbU0aW6Du154r10i1SaimyE16a7yIW1Xz0lBTPdSuShzQ=w323-h399",
         alt: "tic-tac-toe game",
         link: "https://celinesco.github.io/MatcheADAs/",
-        description: "This was an optional project of the ADA bootcamp. It is a 3-match game. Some things are worth improving (cleaning the code + adding some extra functionalities, the responsive design and some bugs), it was really challenging and I am happy with the results.",
+        description: "This was an optional project of the ADA ITW bootcamp. It is a 3-match game. Some things are worth improving (cleaning the code + adding some extra functionalities, the responsive design and some bugs), it was really challenging and I am happy with the results.",
         technologies: "HTML5, CSS3, JS",
         repository: "https://github.com/Celinesco/MatcheADAs"
     },
@@ -35,7 +35,7 @@ export const datosProyecto = [
         img: "https://blogger.googleusercontent.com/img/a/AVvXsEjvvYItYKdgI-8jDOJJTmnqgeEcnZyAer7eBFwDVJIl1uEo3voUNf7k2GeAIvIkReNTkA--PNiZo8gficbgv0pXyRQIYKMoKN3xLxZ9dBZO3Dezde5cH2Vrry5xky6BPff8COKs_DGGHED8876GDMEcAQbbTGqO-1hJTgNff9lonFSY_TgXwUv0bFYdhA=s320",
         alt: "Statistics graph",
         link: "https://celinesco.github.io/AhorrAdas/",
-        description: "The third project of the ADA bootcamp was also a group assignment. It is a tracker for your incomes and expenses. Here we learnt how to store the user information on Local Storage.",
+        description: "The third project of the ADA ITW bootcamp was also a group assignment. It is a tracker for your incomes and expenses. Here we learnt how to store the user information on Local Storage.",
         technologies: "HTML5, BULMA, JS",
         repository: "https://github.com/Celinesco/AhorrAdas",
     },
@@ -65,8 +65,8 @@ export const datosProyecto = [
         title: "Movies",
         img: "https://blogger.googleusercontent.com/img/a/AVvXsEjr85XwtU6erDfZETf5dVTGeQaT6e7DYI61R2NIuB5Hv-jPmMUHN8DdfOK-Ejvmm7QeHaDU_EzT1rYTmHFnytdSRzNkUrGwZfOVGTiH3XxtI_c0vq-MtRQBcx2XW0E4UJ2CpZnGBnRDvHHHBi6MbsFAy5pvg9hcMGMheZklrf3uMBJJyMPk1Cx7W8x4gg=s320",
         alt: "Clapperboard illustration",
-        link: "",
-        description: "",
+        link: "https://infokleinkino.netlify.app/",
+        description: "Last porject of the bootcamp ADA ITW. ",
         technologies: "React.js",
         repository: "https://github.com/Celinesco/buscador_de_peliculas"
     }
