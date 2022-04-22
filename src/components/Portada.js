1856b01adda3fb8797227c5edcdaea929c86b7c8
Celines <celina.alsina@gmail.com>
2022-04-22 19:22:30 +0200

    huellitas animacion portada

---
+++ src/components/Portada.js
@@ -0,0 +1,16 @@
+import './Portada.scss'
+
+
+const Portada = () => {
+    return (
+        <div className="container__portada">
+            <p>Hello,</p>
+            <h1>I'm <span>C</span>elina <span>I</span>nés <span>A</span>lsina</h1>
+            <p>&lt;p&gt;Front-end developer&lt;/p&gt;</p>
+
+        </div >
+    )
+}
+
+
+export default Portada;
