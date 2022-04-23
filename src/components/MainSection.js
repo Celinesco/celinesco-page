0128bf7d0b933a7720478133356e2a03b2c610af
Celines <celina.alsina@gmail.com>
2022-04-23 09:41:33 +0200

    Posicionamiento huellitas

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -3,8 +3,7 @@ import TechnologiesContainer from './SkillsContainer/SkillsContainer';
 import AOS from 'aos';
 import "aos/dist/aos.css";
 import ProjectsContainer from './Projects/ProjectsContainer';
-import { MdPets } from "react-icons/md";
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import Portada from './Portada';
 import About from './About/About'
 
@@ -16,31 +15,11 @@ const MainSection = () => {
         AOS.refresh();
     }, [])
 
-    const [footPrints, setFootPrints] = useState(true);
-
-
-
     return (
 
         <div className="sections">
             <section className='section__main'>
                 <Portada />
-                {footPrints &&
-                    <div className='container__steps'>
-                        <div className='container__step step-1'>
-                            <MdPets />
-                        </div>
-                        <div className='container__step step-2'>
-                            <MdPets />
-                        </div>
-                        <div className='container__step step-3'>
-                            <MdPets />
-                        </div>
-                        <div className='container__step step-4'>
-                            <MdPets />
-                        </div>
-                    </div>}
-
             </section>
             <ProjectsContainer />
             <TechnologiesContainer />
