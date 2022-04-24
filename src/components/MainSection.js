b7dce7094b0a6bb700e6e6b46f0faafdfb2aefba
Celines <celina.alsina@gmail.com>
2022-04-24 12:27:15 +0200

    media query 575.98 seccion skills

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -1,39 +1,38 @@
-import './MainSection.scss';
-import TechnologiesContainer from './SkillsContainer/SkillsContainer';
-import AOS from 'aos';
+import "./MainSection.scss";
+import TechnologiesContainer from "./SkillsContainer/SkillsContainer";
+import AOS from "aos";
 import "aos/dist/aos.css";
-import ProjectsContainer from './Projects/ProjectsContainer';
-import { useEffect } from 'react';
-import Portada from './Portada';
-import About from './About/About';
-import Contact from './Contact/Contact'
-
+import ProjectsContainer from "./Projects/ProjectsContainer";
+import { useEffect } from "react";
+import Portada from "./Portada";
+import About from "./About/About";
+import Contact from "./Contact/Contact";
 
 const MainSection = () => {
-
     useEffect(() => {
         AOS.init();
         AOS.refresh();
-    }, [])
+    }, []);
 
     return (
-
         <div className="sections">
-            <section className='section__main sections'>
+            <section className="section__main sections">
                 <Portada />
             </section>
-            <section className='section__main'>
+            <section className="section__main">
                 <ProjectsContainer />
             </section>
-            <section className='section__main'>
+            <section className="section__main">
                 <About />
             </section>
-            <TechnologiesContainer />
-            <Contact />
-
+            <section className="section__main">
+                <TechnologiesContainer />
+            </section>
+            <section className="section__main">
+                <Contact />
+            </section>
         </div>
-
-    )
-}
+    );
+};
 
 export default MainSection;
